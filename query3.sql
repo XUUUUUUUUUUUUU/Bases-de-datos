aa466fb762917a70fd322760f5e7d90b1c04dcba
alejandro <alejandrozheng666@gmail.com>
2025-10-01 20:02:35 +0200

    update query

--- query3.sql
+++ query3.sql
@@ -1,6 +1,6 @@
 SELECT a.airport_code, COUNT(*) AS total_boardings
 FROM boarding_passes bp
 JOIN flights f ON bp.flight_id = f.flight_id
-JOIN airport_data a ON f.arrival_airport = a.airport_code
+JOIN airports_data a ON f.arrival_airport = a.airport_code
 GROUP BY a.airport_code
 ORDER BY total_boardings ASC;
