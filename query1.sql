903fc283d8bc8c233a4d0d8e890d2da146b052a7
Alejandro-zheng <alejandrozheng666@gmail.com>
2025-10-07 11:50:21 +0200

    Update query1.sql

--- query1.sql
+++ query1.sql
@@ -1,36 +1,44 @@
-WITH first_flight AS(
+WITH flight_times AS (
     SELECT 
-        tf.ticket_no, 
-        MIN(f.scheduled_departure), 
+        tf.ticket_no,
+        MIN(f.scheduled_departure) AS first_dep_time,
+        MAX(f.scheduled_departure) AS last_dep_time
+    FROM ticket_flights tf
+    JOIN flights f ON tf.flight_id = f.flight_id
+    GROUP BY tf.ticket_no
+),
+
+first_flight AS (
+    SELECT 
+        tf.ticket_no,
         f.departure_airport
-    FROM ticket_flights tf 
+    FROM ticket_flights tf
     JOIN flights f ON tf.flight_id = f.flight_id
-    GROUP BY tf.ticket_no, f.departure_airport
+    JOIN flight_times ft ON tf.ticket_no = ft.ticket_no AND f.scheduled_departure = ft.first_dep_time
 ),
 
-last_flight AS(
-    SELECT
-        tf.ticket_no, 
-        MAX(f.scheduled_departure), 
+last_flight AS (
+    SELECT 
+        tf.ticket_no,
         f.arrival_airport
-    FROM ticket_flights tf 
+    FROM ticket_flights tf
     JOIN flights f ON tf.flight_id = f.flight_id
-    GROUP BY tf.ticket_no, f.arrival_airport
+    JOIN flight_times ft ON tf.ticket_no = ft.ticket_no AND f.scheduled_departure = ft.last_dep_time
 ),
 
-round_tickets AS(
-    SELECT
+round_tickets AS (
+    SELECT 
         ff.ticket_no,
         ff.departure_airport
     FROM first_flight ff
-    JOIN last_flight lf ON ff.ticket_no = lf.ticket_no 
+    JOIN last_flight lf ON ff.ticket_no = lf.ticket_no
     WHERE ff.departure_airport = lf.arrival_airport
 )
 
 SELECT 
-    rt.departure_airport, 
-    COUNT(DISTINCT t.book_ref) AS Number_of_bookings
+    rt.departure_airport,
+    COUNT(DISTINCT t.book_ref) AS number_of_bookings
 FROM round_tickets rt
-JOIN tickets t ON t.ticket_no = rt.ticket_no
+JOIN tickets t ON rt.ticket_no = t.ticket_no
 GROUP BY rt.departure_airport
 ORDER BY rt.departure_airport ASC;
