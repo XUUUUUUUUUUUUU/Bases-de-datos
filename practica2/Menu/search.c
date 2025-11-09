6fd78a6fd3f4c7e6111f9fbd28cde5c34ccd657a
alejandro <alejandrozheng666@gmail.com>
2025-11-09 18:20:01 +0100

    changes consulta1

--- practica2/Menu/search.c
+++ practica2/Menu/search.c
@@ -37,6 +37,15 @@ void    results_search(char * from, char *to, char *date,/*añadir date*/
     SQLCHAR free_seats[512];
     SQLCHAR total_duration[512];
 
+    SQLCHAR first_id[512];
+    SQLCHAR second_id[512];
+    SQLCHAR code1[512];
+    SQLCHAR code2[512];
+    SQLCHAR depart1[512];
+    SQLCHAR depart2[512];
+    SQLCHAR arrival1[512];
+    SQLCHAR arrival2[512];
+
     /* CONNECT */
     ret = odbc_connect(&env, &dbc);
     if (!SQL_SUCCEEDED(ret)) {
@@ -60,7 +69,15 @@ void    results_search(char * from, char *to, char *date,/*añadir date*/
 "                  WHERE tf.flight_id = f.flight_id "
 "              ) "
 "        ) AS free_seats, "
-"        (f.scheduled_arrival - f.scheduled_departure) AS total_duration "
+"        (f.scheduled_arrival - f.scheduled_departure) AS total_duration,"
+"			  f.flight_id AS first_flight_id,"
+"			  f.flight_id AS second_flight_id,"
+"			  f.aircraft_code AS first_aircraft_code,"
+"			  f.aircraft_code AS second_aircraft_code,"
+"			  f.scheduled_departure AS first_departure_time,"
+"			  f.scheduled_arrival AS first_arrival_time,"
+"			  f.scheduled_departure AS second_deaparture_time,"
+"			  f.scheduled_arrival AS second_arrival_time "
 " FROM flights f "
 " WHERE f.departure_airport = ? AND f.arrival_airport = ? AND DATE(f.scheduled_arrival) = ? "
 "), "
@@ -92,7 +109,15 @@ void    results_search(char * from, char *to, char *date,/*añadir date*/
 "                  ) "
 "            ) "
 "        ) AS free_seats, "
-"        (f2.scheduled_arrival - f1.scheduled_departure) AS total_duration "
+"        (f2.scheduled_arrival - f1.scheduled_departure) AS total_duration, "
+"			  f1.flight_id AS first_flight_id,"
+"			  f2.flight_id AS second_flight_id,"
+"			  f1.aircraft_code AS first_aircraft_code,"
+"			  f2.aircraft_code AS second_aircraft_code,"
+"			  f1.scheduled_departure AS first_departure_time,"
+"			  f1.scheduled_arrival AS first_arrival_time,"
+"			  f2.scheduled_departure AS second_deaparture_time,"
+"			  f2.scheduled_arrival AS second_arrival_time "
 " FROM flights f1 "
 " JOIN flights f2 ON f1.arrival_airport = f2.departure_airport "
 " WHERE ((f2.scheduled_arrival - f1.scheduled_departure) <= INTERVAL '24 hours') "
@@ -128,11 +153,41 @@ void    results_search(char * from, char *to, char *date,/*añadir date*/
     SQLBindCol(stmt, 5, SQL_C_CHAR, free_seats, sizeof(free_seats), NULL);
     SQLBindCol(stmt, 6, SQL_C_CHAR, total_duration, sizeof(total_duration), NULL);
 
+    SQLBindCol(stmt, 7, SQL_C_CHAR, first_id, sizeof(first_id), NULL);
+    SQLBindCol(stmt, 8, SQL_C_CHAR, code1, sizeof(code1), NULL);
+    SQLBindCol(stmt, 9, SQL_C_CHAR, depart1, sizeof(depart1), NULL);
+    SQLBindCol(stmt, 10, SQL_C_CHAR, arrival1, sizeof(arrival1), NULL);
+    SQLBindCol(stmt, 11, SQL_C_CHAR, second_id, sizeof(second_id), NULL);
+    SQLBindCol(stmt, 12, SQL_C_CHAR, code2, sizeof(code2), NULL);
+    SQLBindCol(stmt, 13, SQL_C_CHAR, depart2, sizeof(depart2), NULL);
+    SQLBindCol(stmt, 14, SQL_C_CHAR, arrival2, sizeof(arrival2), NULL);
+
 
     /* Loop through the rows in the result-set */
     while (row < max_rows && SQL_SUCCEEDED(ret = SQLFetch(stmt))) {
-        snprintf((*choices)[row],max_length,"%-4s  %-20s  %-20s  %s  %-3s  %-8s\n", first_flight,scheduled_departure,scheduled_arrival,connections,free_seats,total_duration);
+
+      if (strcmp((char*)connections, "1") == 0)
+      {
+        snprintf((*choices)[row],max_length,"%-4s  %-20s  %-20s  %s  %-3s  %-8s  \t "
+        "first_flight_id:%-4s first_aricraft_code:%-3s sheduled_departure:%-20s sheduled_arrival:%-20s second_flight_id:%s second_aircraft_code:%s sheduled_departure:%s sheduled_arrival:%s", 
+        first_flight,scheduled_departure,scheduled_arrival,connections,free_seats,total_duration,
+        first_id,code1,depart1,arrival1,second_id,code2,depart2,arrival2);
         row++;
+      }else
+      {
+        snprintf((*choices)[row],max_length,"%-4s  %-20s  %-20s  %s  %-3s  %-8s  \t "
+        "first_flight_id:%-4s first_aricraft_code:%-3s sheduled_departure:%-20s sheduled_arrival:%-20s no hay segundo vuelos", 
+        first_flight,scheduled_departure,scheduled_arrival,connections,free_seats,total_duration,
+        first_id,code1,depart1,arrival1);
+        row++;
+      }
+
+
+      if (row== 0 && max_rows >0)
+      {
+        snprintf((*choices)[0], (size_t)max_length, "No hay vuelo desde %s a %s en %S",from,to,date);
+        *n_choices = 1;
+      }
     }
 
 
